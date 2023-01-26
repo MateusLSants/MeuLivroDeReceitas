07443233ef7efdb3cf87d77738e6850b8d3b37ef
MateusLSants <mateusleandro23@gmail.com>
2023-01-26 08:47:16 -0300

    adicionando endpoint para registrar usuario

--- src/Backend/MeuLivroDeReceitas.Api/MeuLivroDeReceitas.Api/Program.cs
+++ src/Backend/MeuLivroDeReceitas.Api/MeuLivroDeReceitas.Api/Program.cs
@@ -9,6 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddRouting(option => option.LowercaseUrls = true);
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
